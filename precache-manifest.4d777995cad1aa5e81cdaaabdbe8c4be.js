95011478ed61537852e86d06da2a4317c3a7c203
Johnny <johnny@unstoppabledomains.com>
2019-08-19 08:54:52 -0700

    Updates

--- precache-manifest.4d777995cad1aa5e81cdaaabdbe8c4be.js
+++ precache-manifest.4d777995cad1aa5e81cdaaabdbe8c4be.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0f09a46776da38b3c0b3c30675c573d2",
+    "revision": "53153f6b6d7858a1028ecf23024a4fdd",
     "url": "/reseller-demo/index.html"
   },
   {
-    "revision": "ce8bf46d80af202e7040",
+    "revision": "169346c5ae21d1464d0e",
     "url": "/reseller-demo/static/css/main.900c3297.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reseller-demo/static/js/2.fc154770.chunk.js"
   },
   {
-    "revision": "ce8bf46d80af202e7040",
-    "url": "/reseller-demo/static/js/main.1ae49774.chunk.js"
+    "revision": "169346c5ae21d1464d0e",
+    "url": "/reseller-demo/static/js/main.6c4e48ee.chunk.js"
   },
   {
     "revision": "7404beaf926ac4bfe365",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reseller-demo/precache-manifest.1bad5e90f9a37542d4c58c6931301380.js"
+  "/reseller-demo/precache-manifest.4d777995cad1aa5e81cdaaabdbe8c4be.js"
 );
 
 self.addEventListener('message', (event) => {

4cf57d77b341f6edb84784d86f5c768a18ce6010
Johnny <johnny@unstoppabledomains.com>
2019-08-21 02:31:12 -0700

    Updates

---
+++ precache-manifest.f9bdb91770a85e76c62af046206bec77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e7ced3d99704e15b59d7ad432d3b73",
+    "url": "/reseller-demo/index.html"
+  },
+  {
+    "revision": "5868833d2cfea6b52343",
+    "url": "/reseller-demo/static/css/main.27fcb10c.chunk.css"
+  },
+  {
+    "revision": "497bfbcbfa65c5c17720",
+    "url": "/reseller-demo/static/js/2.24c65a4a.chunk.js"
+  },
+  {
+    "revision": "5868833d2cfea6b52343",
+    "url": "/reseller-demo/static/js/main.197a0c74.chunk.js"
+  },
+  {
+    "revision": "7409fee8016773a5c76c",
+    "url": "/reseller-demo/static/js/runtime~main.459a7584.js"
+  }
+]);

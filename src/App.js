3a66ad6e0ad04ae8cabc1836bafc486480ee141b
Johnny <johnny@unstoppabledomains.com>
2019-09-17 18:47:24 -0700

    live domain owner input field

--- src/App.js
+++ src/App.js
@@ -14,115 +14,124 @@ import styles from './styles/app.styles';
 import Stripe from './Pages/Checkout/Stripe';
 
 const App = ({ classes }) => {
-  const [step, setStep] = React.useState(0);
-  const [pointer, setPointer] = React.useState(true);
-  const [domainName, setDomainName] = React.useState('');
-  const [emailProps, setEmailProps] = React.useState({});
-  const [email, setEmail] = React.useState('');
-  const [domainResults, setDomainResults] = React.useState('');
+	const [step, setStep] = React.useState(0);
+	const [pointer, setPointer] = React.useState(true);
+	const [domainName, setDomainName] = React.useState('');
+	const [emailProps, setEmailProps] = React.useState({});
+	const [email, setEmail] = React.useState('');
+	const [owner, setOwner] = React.useState('0xe7474D07fD2FA286e7e0aa23cd107F8379085037');
 
-  useEffect(() => {
-    if (step === 0) {
-      setEmail('');
-      setDomainResults('');
-      setDomainName('');
-      setEmailProps('');
-    }
-  }, [step]);
+	const [domainResults, setDomainResults] = React.useState('');
 
-  const handlePointer = () => {
-    setPointer(!pointer);
-  };
+	useEffect(() => {
+		if (step === 0) {
+			setEmail('');
+			setOwner('0xe7474D07fD2FA286e7e0aa23cd107F8379085037');
+			setDomainResults('');
+			setDomainName('');
+			setEmailProps('');
+		}
+	}, [step]);
 
-  const handleNextStep = () => {
-    if (step < 3) setStep(step + 1);
-  };
+	const handlePointer = () => {
+		setPointer(!pointer);
+	};
 
-  const handlePrevStep = () => {
-    if (step) setStep(step - 1);
-  };
+	const handleNextStep = () => {
+		if (step < 3) setStep(step + 1);
+	};
 
-  const randomizeDomain = () => {
-    setDomainName(
-      `reseller-test-${config.reseller}-${Math.floor(
-        Math.random() * 502562
-      )}.zil`
-    );
-  };
+	const handlePrevStep = () => {
+		if (step) setStep(step - 1);
+	};
 
-  const renderStep = () => {
-    switch (step) {
-      case 0:
-        return (
-          <Search
-            handleNextStep={handleNextStep}
-            handlePrevStep={handlePrevStep}
-            domainName={domainName}
-            step={step}
-            showPointer={pointer}
-            setEmailProps={setEmailProps}
-            setDomainResults={setDomainResults}
-          />
-        );
-      case 1:
-        return (
-          <Search
-            handleNextStep={handleNextStep}
-            handlePrevStep={handlePrevStep}
-            step={step}
-            domainName={domainName}
-            showPointer={pointer}
-            setEmailProps={setEmailProps}
-            setDomainResults={setDomainResults}
-          />
-        );
-      case 2:
-        return (
-          <Email
-            handleNextStep={handleNextStep}
-            emailProps={emailProps}
-            email={email}
-            setEmail={setEmail}
-            step={step}
-            showPointer={pointer}
-            setStep={setStep}
-          />
-        );
-      case 3:
-        return (
-          <Stripe
-            handleNextStep={handleNextStep}
-            handlePrevStep={handlePrevStep}
-            domainObject={emailProps}
-            step={step}
-            showPointer={pointer}
-            setStep={setStep}
-          />
-        );
-      default:
-        break;
-    }
-  };
+	const randomizeDomain = () => {
+		setDomainName(
+			`reseller-test-${config.reseller}-${Math.floor(
+				Math.random() * 502562
+			)}.zil`
+		);
+	};
 
-  console.log('step', step);
+	const renderStep = () => {
+		switch (step) {
+			case 0:
+				return (
+					<Search
+						handleNextStep={handleNextStep}
+						handlePrevStep={handlePrevStep}
+						domainName={domainName}
+						step={step}
+						owner={owner}
+						setOwner={setOwner}
+						showPointer={pointer}
+						setEmailProps={setEmailProps}
+						setDomainResults={setDomainResults}
+					/>
+				);
+			case 1:
+				return (
+					<Search
+						handleNextStep={handleNextStep}
+						handlePrevStep={handlePrevStep}
+						step={step}
+						domainName={domainName}
+						owner={owner}
+						setOwner={setOwner}
+						showPointer={pointer}
+						setEmailProps={setEmailProps}
+						setDomainResults={setDomainResults}
+					/>
+				);
+			case 2:
+				return (
+					<Email
+						handleNextStep={handleNextStep}
+						emailProps={emailProps}
+						email={email}
+						owner={owner}
+						setOwner={setOwner}
+						setEmail={setEmail}
+						step={step}
+						showPointer={pointer}
+						setStep={setStep}
+					/>
+				);
+			case 3:
+				return (
+					<Stripe
+						handleNextStep={handleNextStep}
+						handlePrevStep={handlePrevStep}
+						domainObject={emailProps}
+						step={step}
+						showPointer={pointer}
+						setStep={setStep}
+					/>
+				);
+			default:
+				break;
+		}
+	};
 
-  return (
-    <div className={classes.root}>
-      <Header
-        handlePointer={handlePointer}
-        pointer={pointer}
-        step={step}
-        randomizeDomain={randomizeDomain}
-        setEmail={setEmail}
-        isDomainLive={
-          domainResults.domain && domainResults.domain.reselling
-            ? !domainResults.domain.reselling.test
-            : false
-        }
-      />
-      <div className={classes.layout}>
-        {renderStep()}
-        {/* <Router history={history} basename="/">
+	console.log('step', step);
+
+	return (
+		<div className={classes.root}>
+			<Header
+				handlePointer={handlePointer}
+				pointer={pointer}
+				step={step}
+				randomizeDomain={randomizeDomain}
+				setEmail={setEmail}
+				isDomainLive={
+					domainResults.domain && domainResults.domain.reselling
+						? !domainResults.domain.reselling.test
+						: false
+				}
+			/>
+			<div className={classes.layout}>
+				{renderStep()}
+				{/* <Router history={history} basename="/">
           <Switch>
             <Route path="/" exact component={Search} />
             <Route path="/reseller-demo/" exact component={Search} />
@@ -132,9 +141,9 @@ const App = ({ classes }) => {
             <Route path="/send" exact component={Send} />
           </Switch>
         </Router> */}
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 };
 
 export default withStyles(styles)(App);
--- src/Pages/Email/Email.js
+++ src/Pages/Email/Email.js
@@ -1,115 +1,139 @@
 import React, { useState } from 'react';
 import Pointer from '../../Utilities/Pointer';
 import {
-  Paper,
-  withStyles,
-  Typography,
-  Button,
-  InputBase
+	Paper,
+	withStyles,
+	Typography,
+	Button,
+	InputBase
 } from '@material-ui/core';
 import styles from '../../styles/email.styles';
 import SearchIcon from '@material-ui/icons/Search';
+import helper from '../../Utilities/Helpers';
 
 const Email = ({
-  classes,
-  emailProps,
-  email,
-  setEmail,
-  step,
-  showPointer,
-  setStep
+	classes,
+	emailProps,
+	email,
+	owner,
+	setOwner,
+	setEmail,
+	step,
+	showPointer,
+	setStep
 }) => {
-  const [emailError, setEmailError] = useState('');
-  const isEmailValid = e => {
-    // eslint-disable-next-line
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      e
-    );
-  };
+	const [emailError, setEmailError] = useState('');
+	const isEmailValid = e => {
+		// eslint-disable-next-line
+		return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+			e
+		);
+	};
 
-  const handleSubmit = () => {
-    console.log('submitting form');
-    if (isEmailValid(email)) {
-      setStep(3);
-    } else {
-      setEmailError('Incorrect email');
-    }
-  };
 
-  const handleEmailChange = e => {
-    if (emailError) {
-      setEmailError('');
-    }
-    setEmail(e.target.value);
-  };
+	const handleSubmit = () => {
+		console.log('submitting form');
+		if (helper.isAddress(owner) && isEmailValid(email)) {
+			setStep(3);
+		} else if (!helper.isAddress(owner)) {
+			setEmailError('Incorrect owner format')
+		}
+		else {
+			setEmailError('Incorrect email');
+		}
+	};
 
-  return (
-    <Paper className={classes.paper}>
-      <div className={classes.headerDiv}>
-        <Typography variant="h5" className={classes.bold}>
-          Enter Email
+	const handleEmailChange = e => {
+		if (emailError) {
+			setEmailError('');
+		}
+		setEmail(e.target.value);
+	};
+
+	return (
+		<Paper className={classes.paper}>
+			<div className={classes.headerDiv}>
+				<Typography variant="h5" className={classes.bold}>
+					Enter Email
         </Typography>
-        <Button
-          color="primary"
-          className={classes.searchButton}
-          onClick={() => {
-            setStep(0);
-          }}
-        >
-          <SearchIcon className={classes.searchIcon} />
-          Search new
+				<Button
+					color="primary"
+					className={classes.searchButton}
+					onClick={() => {
+						setStep(0);
+					}}
+				>
+					<SearchIcon className={classes.searchIcon} />
+					Search new
         </Button>
-      </div>
-      <div className={classes.domainDiv}>
-        <div>
-          <Typography className={classes.bold}>
-            {emailProps.domain.name.split('.')[0]}
-            <span className={classes.extension}>
-              .{emailProps.domain.name.split('.')[1]}
-            </span>
+			</div>
+			<div className={classes.domainDiv}>
+				<div>
+					<Typography className={classes.bold}>
+						{emailProps.domain.name.split('.')[0]}
+						<span className={classes.extension}>
+							.{emailProps.domain.name.split('.')[1]}
+						</span>
+					</Typography>
+					<Typography variant="subtitle2" color="textSecondary">
+						Domain is available
           </Typography>
-          <Typography variant="subtitle2" color="textSecondary">
-            Domain is available
-          </Typography>
-        </div>
-        <Typography color="primary" className={classes.bold}>
-          $ {emailProps.domain.reselling.price}.00
+				</div>
+				<Typography color="primary" className={classes.bold}>
+					$ {emailProps.domain.reselling.price}.00
         </Typography>
-      </div>
-      <Typography className={classes.lessBold}>
-        Please provide an email address
+			</div>
+			<Typography className={classes.lessBold}>
+				Please provide an email address
       </Typography>
-      <div className={classes.inputDiv}>
-        {step === 2 && showPointer ? (
-          <div style={{ position: 'fixed', transform: 'translateX(-40px)' }}>
-            <Pointer />
-          </div>
-        ) : null}
-        <InputBase
-          placeholder="Your Email"
-          className={classes.input}
-          value={email}
-          onChange={handleEmailChange}
-          onKeyDown={e => (e.key === 'Enter' ? handleSubmit() : null)}
-        />
-      </div>
-      {emailError ? (
-        <div className={classes.errorDiv}>
-          <Typography color="error" className={classes.errorMessage}>
-            {emailError}
-          </Typography>
-        </div>
-      ) : null}
-      <Button
-        onClick={() => handleSubmit()}
-        color="primary"
-        variant="contained"
-        className={classes.button}
-        disabled={!email}
-      >
-        Next
+			<div className={classes.inputDiv}>
+				{step === 2 && showPointer ? (
+					<div style={{ position: 'fixed', transform: 'translateX(-40px)' }}>
+						<Pointer />
+					</div>
+				) : null}
+				<InputBase
+					placeholder="Your Email"
+					className={classes.input}
+					value={email}
+					onChange={handleEmailChange}
+					onKeyDown={e => (e.key === 'Enter' ? handleSubmit() : null)}
+				/>
+			</div>
+			{emailError ? (
+				<div className={classes.errorDiv}>
+					<Typography color="error" className={classes.errorMessage}>
+						{emailError}
+					</Typography>
+				</div>
+			) : null}
+
+			{emailProps.owner === '' ?
+				<>
+					<Typography className={classes.lessBold}>
+						Owner crypto address
+      		</Typography>
+					<div className={classes.inputDiv}>
+						<InputBase
+							placeholder="Your ETH or ZIL address"
+							className={classes.input}
+							value={owner}
+							onChange={(e) => setOwner(e.target.value)}
+							onKeyDown={e => (e.key === 'Enter' ? handleSubmit() : null)}
+						/>
+					</div>
+				</>
+				: null}
+			<Button
+				onClick={() => handleSubmit()}
+				color="primary"
+				variant="contained"
+				className={classes.button}
+				disabled={!email}
+			>
+				Next
       </Button>
-    </Paper>
-  );
+		</Paper>
+	);
 };
 export default withStyles(styles)(Email);
